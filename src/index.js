c928f790fccf6a320d2117f4a5df68a4f01a7da9
cookkeys <snitin.imk@gmail.com>
2024-10-01 22:57:54 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import connectDB from "./db/index.js";
 import 'dotenv/config';
+import {app} from "./app.js"
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(` Server is running at port: ${process.env.port}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!", err);
+})
