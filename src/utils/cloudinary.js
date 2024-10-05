df1c71d0adc85fc3993a20779fe67364b758bfd8
cookkeys <snitin.imk@gmail.com>
2024-10-05 16:42:20 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,6 +13,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type:"auto"
         })
+        //console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
