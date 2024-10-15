2bfa35a0a868cf5e1284c74e8bc9d24dfa661a3a
cookkeys <snitin.imk@gmail.com>
2024-10-15 16:58:44 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {    changeCurrentPassword,
+            getCurrentUser,
+            getUserChannelProfile,
+            getWatchHistory,
+            registerUser,
+            updateAccountDetails,
+            updateUserAvatar,
+            updateUserCoverImage
+         } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { loginUser } from "../controllers/user.controller.js";
 import { logoutUser } from "../controllers/user.controller.js";
@@ -27,6 +35,15 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken) //add route for refreshing the token
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/current-user").post(verifyJWT, getCurrentUser)   // used for as we don't want to update all details
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)    // .patch as we are expecting avatar file
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile) // /c/ when taking data from params 
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
